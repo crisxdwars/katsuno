510f5af21309094dd69c0203b9b8059318706e69
= <=>
2026-06-07 15:37:45 +0800

    Initial Release 5.1: Colored Title & Removed Script.

--- src/scripts/script.js
+++
@@ -1,8 +0,0 @@
-const a1a1 = document.getElementById('toast');
-
-function e_button(a1a1, toastClose, delayMs = 2000) {
-    const open = a1a1.classList.toggle('toastClose');
-    if (open) {
-        setTimeout(() => a1a1.classList.remove('toastClose'), delayMs);
-    }
-}
